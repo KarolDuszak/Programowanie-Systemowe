a64477e2431e8731ee720854cb88e0e6097a60bd
KarolDuszak <kl.duszak@gmail.com>
2022-04-10 14:04:40 +0200

    Lab1 Finished

--- LAB1/zadanie3.c
+++ LAB1/zadanie3.c
@@ -5,6 +5,8 @@ int main(){
     unsigned char *p1 = malloc(10*sizeof(unsigned char));
     unsigned char *p2 = malloc(20*sizeof(unsigned char));
     memset(p1, 1, 10);
-    p1[10]=2;
+    p1[9]=2;
+    free(p1);
+    free(p2);
     return 0;
 }

2982b28ea213fd0a64ae6dd9888d10fcc2b1dd63
KarolDuszak <kl.duszak@gmail.com>
2022-04-24 12:58:59 +0200

    geting options

--- LAB4/44312.ps.lab04.main.c
+++ LAB4/44312.ps.lab04.main.c
@@ -4,22 +4,52 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, char **argv)
 {
     int fv=0, ft=0;
     int opt;
-    while ((opt = getopt(argc, argv, "vt:")) != -1)
+
+    for(int i=0; i< argc ; i++)
     {
+        printf("%s \n", argv[i]);
+    }
+    while ((opt = getopt(argc, argv, "+vt:")) != -1)
+    {
+        //char x = opt;
+        //printf("%c \n",x);
         switch (opt)
         {
         case 'v':
             fv = 1;
+            printf("flaga v");
             break;
         case 't':
+        //Konwersja na int ze stringa
+    
             ft = 1;
+            char* tvalue = optarg;
+            long tNumber = strtol(tvalue, NULL, 10);
+
+            tNumber = tNumber +1;
+            printf("flaga t %s %ld", tvalue, tNumber);
             break;      
         }
     }
     return 0;
+}
+
+int isNumber(char *number)
+{
+    size_t len = sizeof(number)/sizeof(number[0]);
+
+    for(int i=0; i<len; i++)
+    {
+        if(!isdigit(number[i]))
+        {
+            return 0;
+        }
+    }
+    return 1;
 }

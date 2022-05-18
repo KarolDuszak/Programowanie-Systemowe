0ca929857ca66cff0493afe61aac72a0a0edaf58
KarolDuszak <kl.duszak@gmail.com>
2022-05-18 12:09:37 +0200

    system call dziala srednio

--- LAB4/testprog.c
+++ LAB4/testprog.c
@@ -10,10 +10,48 @@
 #include <time.h>
 #include <sys/wait.h>
 #include <sys/resource.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
 
-void printMessageToStdErr()
+void userSpace()
 {
-    
+    pid_t pid;
+    pid = fork();
+    printf("Pid: %d PPid: %d\n", getpid(), getppid());
+}
+
+void systemSpace()
+{
+    FILE *file;
+    char buf[20];
+    //clear file
+    fclose(fopen("fileToWrite.txt", "w"));
+
+    file =fopen("fileToWrite.txt", "w+");
+    for(int i=0; i<10 ;i++)
+    {
+        fprintf(file, "Line %d added \n", i);
+    }
+    fclose(file);
+
+    int fd;
+    fd =open("fileToWrite.txt", O_RDONLY);
+    read(fd,buf,20);
+    //sleep(1);
+    close(fd);
+    printf("buf: %s\n", buf);
+
+    // pid_t pid;
+    // pid=fork();
+    // int wstatus;
+    // if(pid ==-1)
+    // {
+    //     fprintf(stderr,"Somethink went wrong on fork");
+    // }
+    // else if(pid>0)
+    // {
+    //     wait(&wstatus);
+    // }
 }
 
 int main(int argc, char **argv)
@@ -24,21 +62,21 @@ int main(int argc, char **argv)
 
     while ((opt = getopt(argc+1, argv, "eus")) != -1)
     {
-        printf("%d\n",opt);
         switch (opt)
         {
             case 'e':
                 fprintf(stderr, "\nThis is message printed to sdterr\n");
                 break;
             case 'u':
-                fu=1;
+                userSpace();
                 break;
             case 's':
-                fs=1;
+                systemSpace();
                 break;
             default:
                 break;
         }
     }
-
+    
+    printf("\n");
 }

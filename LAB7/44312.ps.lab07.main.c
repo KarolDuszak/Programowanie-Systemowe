43d55dc8b458d80100e4951856318532ae853a2c
KarolDuszak <kl.duszak@gmail.com>
2022-05-27 12:56:02 +0200

    zad 2 init

---
+++ LAB7/44312.ps.lab07.main.c
@@ -0,0 +1,62 @@
+// PS IN1 320 LAB07
+// Karol Duszak
+// dk44312@zut.edu.pl
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <crypt.h>
+#include <unistd.h>
+
+int isNumber(char *number)
+{
+    size_t len = sizeof(number)/sizeof(number[0]);
+
+    for(int i=0; i<len; i++)
+    {
+        if(!isdigit(number[i]))
+        {
+            if(number[i] == 0)
+            {
+                break; //When it is null then all passed params were checked
+            }
+            //printf("This is not a digit: %d\n", number[i]);
+            return 0;
+        }
+        //printf("This is a digit %c\n", number[i]);
+    }
+    return 1;
+}
+
+int main(int argc, char **argv)
+{
+    char* hash=NULL;
+    char* pathToFile=NULL;
+    int threadsNumber = 1;
+    int opt;
+
+    while ((opt = getopt(argc, argv, "s:p:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'h':
+            hash = optarg;
+            break;
+        case 'p':
+            pathToFile = optarg;
+            break;
+        case 't':
+                if(isNumber(optarg)==0)
+                {
+                    printf("Passed value for switch t is not a number: %s closing program\n", optarg);
+                    exit(1);
+                }
+                char* cvalue = optarg;
+                threadsNumber = strtol(cvalue, NULL, 10);
+            break;
+        }
+    }
+
+    printf("Hash: %s pahtToFile: %s threads: %d\n", hash, pathToFile, threadsNumber);
+
+}
